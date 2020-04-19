bde6c8c520af4e23b22b43a28b37b2849893de30
Jonathan-Odia Osahon Emmanuel <emmyodia@gmail.com>
2020-04-19 23:17:17 +0100

    db connection in heroku update

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : 'postgresql-amorphous-43155',
-    user : 'aneagoie',
-    password : '',
-    database : 'smart-brain'
+    host : process.env.DATABASE_URL,
+    ssl: true
   }
 });
 
