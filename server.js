2b9fb354efc6925babe858ca61584f273c9b091e
Jonathan-Odia Osahon Emmanuel <emmyodia@gmail.com>
2020-04-19 23:05:06 +0100

    db route

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-amorphous-43155',
     user : 'aneagoie',
     password : '',
     database : 'smart-brain'
